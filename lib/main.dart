e6e678ab7660d4624a0d91cd3ad52ff2d1d3f15e
Lichyo <lichyo003@gmail.com>
2024-02-10 00:06:00 +0800

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'controller.dart';
 import 'package:flutter/material.dart';
+import 'painter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,52 +8,84 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   Controller controller = Controller();
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Magic Conch Shell'),
-          backgroundColor: Colors.purple[200],
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                'image/${controller.image}',
-                width: 300,
-                height: 300,
-              ),
-              Text(
-                controller.answer,
-                style: const TextStyle(
-                  fontSize: 30.0,
+        body: Stack(
+          children: [
+            Image.asset(
+              'image/background.png',
+              fit: BoxFit.fill,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 200.0,
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            controller.getAnswer();
-            setState(() {});
-          },
-          backgroundColor: Colors.purple,
-          child: const Icon(
-            Icons.question_mark,
-            color: Colors.white,
-          ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Stack(
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          'image/${controller.image}',
+                          width: 300,
+                        ),
+                      ),
+                      Image.asset('image/television.png'),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Transform.rotate(
+                  angle: 180 / 3.14,
+                  child: Draggable(
+                    onDragStarted: () {
+                      controller.getAnswer();
+                    },
+                    onDragEnd: (value) {
+                      setState(() {});
+                    },
+                    feedback: Image.asset(
+                      'image/tab.png',
+                      width: 200,
+                    ),
+                    childWhenDragging: Image.asset(
+                      'image/shell.png',
+                      width: 200,
+                    ),
+                    child: Image.asset(
+                      'image/origin_shell.png',
+                      width: 200,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/painter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Painter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
